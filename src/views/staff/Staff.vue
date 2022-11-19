28017a0b8f334d98db6d048e54a6fd5b12b339db
SenpaiVR <SenpaiShichi@gmail.com>
2022-11-18 20:16:58 -0500

    Merge Staff and Directors into a single one page app

--- src/views/staff/Staff.vue
+++ src/views/staff/Staff.vue
@@ -1,9 +1,73 @@
 <template>
 	<FullBack>
 		<ColumnLayout class="py-16">
-		<div>
-			<SectionBanner text="Staff Directory" class="py-16" />
-		</div>
+			<div>
+				<SectionBanner text="Staff Directory" class="py-16" />
+				<v-card
+					style = "background-color: rgba(0,0,0,0)"
+					flat
+					tile
+				>
+					<v-card-actions class="justify-space-between" >
+							<v-btn
+								text
+								@click="prev"
+								color = white
+							>
+								<v-icon>mdi-chevron-left</v-icon>
+							</v-btn>
+							<v-item-group
+								v-model="onboarding"
+								class="text-center"
+								mandatory
+							>
+								<v-item
+									v-for="n in length"
+									:key="`btn-${n}`"
+									v-slot="{ active, toggle }"
+								>
+									<v-btn
+										:input-value="active"
+										@click="toggle"
+										color = "secondary"
+									>
+										<v-icon v-if = "n == 1" >Directors</v-icon>
+										<v-icon v-if = "n == 2" >Teams</v-icon>
+									</v-btn>
+								</v-item>
+							</v-item-group>
+							<v-btn
+								text
+								@click="next"
+								color = white
+							>
+								<v-icon>mdi-chevron-right</v-icon>
+							</v-btn>
+					</v-card-actions>
+					<v-window
+						v-model="onboarding"
+					>
+						<v-window-item
+							v-for="n in length"
+							:key="`card-${n}`"
+						>
+							<!-- Director Card -->
+							<v-card v-if = "n == 1"  
+							style = "background-color: rgba(0,0,0,0)"
+							>
+								<DirectorsStaffView/>
+							</v-card>
+							
+							<!-- Team Card  -->
+							<v-card v-if = "n == 2" 
+							style = "background-color: rgba(0,0,0,0)"
+							>
+								<TeamsStaffView />
+							</v-card>
+						</v-window-item>
+					</v-window>
+				</v-card>
+			</div>
 		</ColumnLayout>
 	</FullBack>
 </template>
@@ -15,6 +79,8 @@
 import FullBack from "@/components/FullBack.vue";
 import ColumnLayout from "@/components/ColumnLayout.vue";
 import SectionBanner from "@/components/SectionBanner.vue";
+import DirectorsStaffView from '@/views/staff/Directors.vue';
+import TeamsStaffView from '@/views/staff/TeamsMain'
 
 export default {
 	mounted() {
@@ -22,12 +88,30 @@ export default {
 	},
 	name: "StaffView",
 	components: {
-		FullBack,
-		ColumnLayout,
-		SectionBanner
-	},
+    FullBack,
+    ColumnLayout,
+    SectionBanner,
+		DirectorsStaffView,
+		TeamsStaffView
+},
 	props: [],
-	data: () => ({}),
-	methods: {},
+	data: () => ({
+		length: 2,
+		onboarding: 0,
+
+	}),
+	methods: {
+		next () {
+			this.onboarding = this.onboarding + 1 === this.length
+				? 0
+          : this.onboarding + 1
+		},
+		prev () {
+			this.onboarding = this.onboarding - 1 < 0
+				? this.length - 1
+				: this.onboarding - 1
+		},
+	},
+	computed: {}
 };
 </script>
